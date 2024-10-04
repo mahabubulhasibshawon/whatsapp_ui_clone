0f516b2192d644858505b06ffc36b7dfff104ea1
mahabubulhasibshawon <mahabubulhasibshawon@gmail.com>
2024-10-04 21:00:00 +0600

    initial commit

---
+++ android/app/src/main/kotlin/com/hasib/whatsapp_ui_clone/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.hasib.whatsapp_ui_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,39 @@
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 5),(){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomePage()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.network('')
+          ],
+        ),
+      ),
+    );
+  }
+}
